e4e23c5dc5fdc062338096c066a53814aa8dab1a
Ben Robinson <4210111+bsrobinson@users.noreply.github.com>
2025-07-16 20:46:07 +0100

    Fix special episode matching bug (Zog)

--- MediaLife/Models/ClientData.cs
+++ MediaLife/Models/ClientData.cs
@@ -95,7 +95,7 @@ namespace MediaLife.Models
                                     }
                                     else
                                     {
-                                        Episode = Show.Episodes.FirstOrDefault(e => e.SeriesNumber == seriesNumber && fileName.ToLower().Contains(e.Name.ToLower()) && e.SiteSection == SiteSection.TV).DeepClone();
+                                        Episode = Show.Episodes.OrderByDescending(e => e.Name.Length).FirstOrDefault(e => e.SeriesNumber == seriesNumber && fileName.ToLower().Contains(e.Name.ToLower()) && e.SiteSection == SiteSection.TV).DeepClone();
                                     }
 
                                     if (Episode != null)
